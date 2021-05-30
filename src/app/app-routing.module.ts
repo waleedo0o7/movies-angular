317b2dfc66912b303f3cbff57ac14c4521007f7b
waleedo0o7 <waleedhd1@gmail.com>
2021-05-30 19:17:32 +0200

    add popular movies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
+import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
 
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { TrendingMoviesComponent } from './trending-movies/trending-movies.component';
@@ -10,8 +11,9 @@ const routes: Routes = [
   { path : '' , redirectTo : 'movies' , pathMatch : 'full'},
   { path : 'movies' , component : MoviesComponent},
   { path : 'movie-details' , component : MovieDetailsComponent},
-  { path : 'search' , component : SearchResultsComponent},
-  {path : 'trending-movies' , component : TrendingMoviesComponent }
+  { path : 'popular-movies' , component : PopularMoviesComponent },
+  { path : 'trending-movies' , component : TrendingMoviesComponent },
+  { path : 'search' , component : SearchResultsComponent}
 ];
 
 @NgModule({
